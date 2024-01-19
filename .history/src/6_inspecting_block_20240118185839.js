df71a15aca323296fe267408f5efd42ac171eb3b
zzm <zzm12990@gmail.com>
2024-01-19 15:31:59 +0800

    inspect block

---
+++ .history/src/6_inspecting_block_20240118185839.js
@@ -0,0 +1,25 @@
+const {ethers} = require("ethers");
+
+const INFURA_ID = "6a10c6a6a3b44e1e8f2c4671e7a34319";
+
+const provider = new  ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${INFURA_ID}`);
+
+
+const main = async ()=>{
+    const block = await provider.getBlockNumber();
+
+    console.log(`\n Block Number: ${block}\n`);
+
+    const blockInfo = await provider.getBlock(block);
+
+    console.log(blockInfo);
+
+    const {transactions} = await provider.getBlockWithTransactions(block);
+
+    console.log(`\nLogging first transaction in block :\n`);
+
+    console.log(transactons);
+
+}
+
+main();
