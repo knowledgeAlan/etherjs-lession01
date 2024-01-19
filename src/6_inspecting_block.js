0da5b829280de411d714487684f2ca7b2ea996e7
Farmer <1299076979@qq.com>
2024-01-19 15:34:31 +0800

    remove senstive

--- src/6_inspecting_block.js
+++ src/6_inspecting_block.js
@@ -1,6 +1,6 @@
 const {ethers} = require("ethers");
 
-const INFURA_ID = "6a10c6a6a3b44e1e8f2c4671e7a34319";
+const INFURA_ID = "";
 
 const provider = new  ethers.providers.JsonRpcProvider(`https://goerli.infura.io/v3/${INFURA_ID}`);
 
